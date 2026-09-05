b4320764d31d04da09076a3adcf02e0c163d8613
Paul Hill <zikalify@gmail.com>
2026-09-05 17:43:14 +0800

    Animations
    Added animation to daily remaining bar and tried to fix cut off issues

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budget-tool-web-v21';
+const CACHE = 'budget-tool-web-v25';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/money-bags.svg'];
 
 self.addEventListener('install', event => {
