7b6e8d1802403a29ee403b31ae1ef0026b8dd8ca
Paul Hill <zikalify@gmail.com>
2026-09-05 19:00:28 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budget-tool-web-v36';
+const CACHE = 'budget-tool-web-v35';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/money-bags.svg'];
 
 self.addEventListener('install', event => {
