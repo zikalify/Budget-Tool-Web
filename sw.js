83c40f29cb7548bd45799c8c8df92db9f5433a84
Paul Hill <zikalify@gmail.com>
2026-09-08 14:40:21 +0800

    New icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budget-tool-web-v50';
+const CACHE = 'budget-tool-web-v51';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/money-bags.svg'];
 
 self.addEventListener('install', event => {
