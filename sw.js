2643ab5c8d27ed2c9a0d995ada728d8802bff825
Paul Hill <zikalify@gmail.com>
2026-09-13 23:16:53 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Budget Tool Web — licensed under the GNU General Public License v3.0.
 // See LICENSE for the full text.
-const CACHE = 'budget-tool-web-v100';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/money-bags.svg', './fonts/fonts.css', './fonts/roboto-variable.woff2', './fonts/roboto-variable-ext.woff2', './fonts/roboto-flex-variable.woff2', './fonts/roboto-flex-variable-ext.woff2'];
+const CACHE = 'budget-tool-web-v101';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/money-bags.svg', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable-512.png', './fonts/fonts.css', './fonts/roboto-variable.woff2', './fonts/roboto-variable-ext.woff2', './fonts/roboto-flex-variable.woff2', './fonts/roboto-flex-variable-ext.woff2'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
